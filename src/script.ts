8be3f394d99745efa9d44695e1f3b29fc4d783cd
Никитин Руслан Романович <RRNikitin@alfabank.ru>
2025-03-09 12:32:24 +0300

    better funcs logging

--- src/script.ts
+++ src/script.ts
@@ -6,18 +6,37 @@ import {
 } from "./clear-funcs";
 import { products } from "./data";
 
+/// --- Пункт 1.1.
+
 // Пример использования функции filterMultiples
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("--> numbers: ");
+console.log(numbers);
+
 const multiplesOf2 = filterMultiples(numbers, 2);
+console.log("--> : filterMultiples(numbers, 2)");
 console.log(multiplesOf2); // [2, 4, 6, 8, 10]
+
+/// --- Пункт 1.2.
 
 // Пример использования функции joinStrings
 const strings = ["Hello", "World", "TypeScript"];
+console.log("--> strings: ");
+console.log(strings);
+
 const joinedString = joinStrings(strings, " ");
+console.log("--> joinStrings(strings, ' '): ");
 console.log(joinedString); // "Hello World TypeScript"
 
+/// --- Пункт 1.3.
+
+// Пример использования функции sortByProperty
+console.log("--> products: ");
+console.table(products);
+
 const sortedByPrice = sortByProperty(products, "price");
-console.log(sortedByPrice);
+console.log("--> : sortByProperty(products, 'price')");
+console.table(sortedByPrice);
 // [
 //   { id: 3, name: "Headphones", price: 150, ... },
 //   { id: 5, name: "Smartwatch", price: 250, ... },
@@ -26,11 +45,18 @@ console.log(sortedByPrice);
 //   { id: 1, name: "Laptop", price: 1200, ... }
 // ]
 
+/// --- Пункт 2.
+
 // Пример использования функции withLogging
 const add = (a: number, b: number) => a + b;
+console.log("--> add: ");
+console.log(add.toString());
+
 const addWithLogging = withLogging(add);
 const sum = addWithLogging(3, 4);
 // В консоли будет:
 // Calling function with arguments: [3,4]
 // Function returned: 7
+
+console.log("--> sum: ");
 console.log(sum); // 7
